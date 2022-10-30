251761d718b3775747b112473886b6fbaeac8336
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-10-30 22:25:09 +0600

    insertion sort to sorting array

---
+++ Algorithm exercise/6_insertion_sort_to_sorting_array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = { 5, 1, 4, 2, 8};
+    int N = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, N);
+    cout << "Sorted array: \n";
+
+    for ( int i = 0; i < N; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
