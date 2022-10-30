d27860aebba42b36a6a1bf3beacb120b5e8a83e0
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-10-30 21:56:48 +0600

    count the characters in a string

--- Algorithm exercise/4_count_the_characters_in_a_string.cpp
+++ Algorithm exercise/4_count_the_characters_in_a_string.cpp
@@ -1,1 +1,12 @@
 //Write a program that can count the characters in a string.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //string str="Bangladesh"
+    string str;
+    cin>>str;
+    int len=str.length();
+    cout<<"Input string: "<<str<<endl;
+    cout<<"Total Character in the string: "<<len<<endl;
+}
