a79abd2b6656fb0208859f06c73d9b07f016c1fd
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-11-02 22:26:36 +0600

    remove the bubble sort func

--- Algorithm exercise/5_bubble_sort.cpp
+++ Algorithm exercise/5_bubble_sort.cpp
@@ -1,25 +1,20 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n - 1; i++)
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
-}
-
 int main()
 {
     int arr[] = { 5, 1, 4, 2, 8};
     int N = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, N);
-    cout << "Sorted array: \n";
+    int i, j;
 
+    for (i = 0; i < N - 1; i++)
+        for (j = 0; j < N - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+
+    cout << "Sorted array: \n";
     for ( int i = 0; i < N; i++)
         cout << arr[i] << " ";
-    cout << endl;
 
     return 0;
 }
